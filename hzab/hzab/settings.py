0487d89789d8eb769ba0d49fb667a1b4cf529a3a
surafel-cpu <masamoyosef@gmail.com>
2026-02-08 18:47:32 +0300

    2ndbatman

--- hzab/hzab/settings.py
+++ hzab/hzab/settings.py
@@ -127,5 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 ACCOUNT_USERNAME_BLACKLIST = ['admin','accounts','profile','category','post','inbox','spiderman']
