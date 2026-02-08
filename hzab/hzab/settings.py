f83226fd1abc7b599264d6b5590aed1d1962e363
surafel-cpu <masamoyosef@gmail.com>
2026-02-08 23:10:20 +0300

    after_no_css_live

--- hzab/hzab/settings.py
+++ hzab/hzab/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,5 +133,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 ACCOUNT_USERNAME_BLACKLIST = ['admin','accounts','profile','category','post','inbox','spiderman']
